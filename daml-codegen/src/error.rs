194375880f2899164c0270a8f8ac75e0f8556f2b
drsk <drsk0@proton.me>
2026-07-06 17:40:29 +0200

    daml-codegen: derive thiserror::Error on DamlCodeGenError

--- daml-codegen/src/error.rs
+++ daml-codegen/src/error.rs
@@ -1,28 +1,17 @@
 use daml_lf::DamlLfError;
-use std::fmt;
-use std::fmt::{Display, Formatter};
 
 /// Daml code generator errors.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum DamlCodeGenError {
-    /// AN invalid module matcher regex was provided.
-    InvalidModuleMatcherRegex(regex::Error),
+    /// An invalid module matcher regex was provided.
+    #[error("invalid module matcher regex: {0}")]
+    InvalidModuleMatcherRegex(#[from] regex::Error),
     /// Daml LF error.
-    DamlLfError(DamlLfError),
+    #[error("Daml LF error: {0}")]
+    DamlLfError(#[from] DamlLfError),
     /// IO error.
-    IoError(std::io::Error),
-}
-
-impl std::error::Error for DamlCodeGenError {}
-impl Display for DamlCodeGenError {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            DamlCodeGenError::InvalidModuleMatcherRegex(e) =>
-                write!(fmt, "InvalidModuleMatcherRegex {}", (e as &regex::Error)),
-            DamlCodeGenError::DamlLfError(e) => write!(fmt, "Daml LF error {}", (e as &DamlLfError)),
-            DamlCodeGenError::IoError(e) => write!(fmt, "IOError: {}", (e as &std::io::Error)),
-        }
-    }
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
 }
 
 /// Daml code generator result.
